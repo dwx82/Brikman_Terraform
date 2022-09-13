3b9c2c2ae325c47bd35d9d386bd4bd826af02c74
Vadim Anpilogov <vadimanpilogov@MacBook-Pro-Vadim.local>
2022-09-13 13:40:47 +0300

    modules

---
+++ Stage/data-stores/mysql/main.tf
@@ -0,0 +1,44 @@
+############################################################################
+
+provider "aws" {
+        region = "us-east-1"
+}
+
+############################################################################
+
+resource "aws_db_instance" "example" {
+    identifier_prefix   = "terraform-up-and-running"
+    engine              = "mysql"
+    allocated_storage   = 10
+    instance_class      = "db.t2.micro"
+    db_name             = "example_database"
+    username            = "admin"
+    password            = local.db_pass.password
+    skip_final_snapshot = true
+}
+
+data "aws_secretsmanager_secret_version" "db_password" {
+    # Fill in the name you gave to your secret
+    secret_id = "mysql_password_stage"
+}
+
+locals {
+  db_pass = jsondecode(
+    data.aws_secretsmanager_secret_version.db_password.secret_string
+  )
+}
+############################################################################
+
+terraform {
+    backend "s3" {
+    # Поменяйте это на имя своего бакета!
+    bucket = "terraform-state-brickman"
+    key = "stage/datastores/mysql/terraform.tfstate"
+    region = "us-east-1"
+    #Замените это именем своей таблицы DynamoDB!
+    dynamodb_table = "terraform-state-brickman"
+    encrypt = true
+    }
+}
+
+############################################################################
